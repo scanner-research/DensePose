34efbd4f68924fd2cbc33f3609f5b68153f28a15
haotianz <haotianz@stanford.edu>
2019-05-03 16:53:37 -0700

    checkpoint for python3

--- docker/launch_docker.sh
+++ docker/launch_docker.sh
@@ -1,9 +1,9 @@
 nvidia-docker build -f docker/Dockerfile -t densepose:py35 .
 
-nvidia-docker run -ti --ipc=host -v $(pwd):/DensePose-py2 --workdir=/DensePose-py2 -p 8888:8888 densepose:py35 /bin/bash
+nvidia-docker run -ti --ipc=host -v $(pwd):/DensePose --workdir=/DensePose -p 8888:8888 densepose:py35 /bin/bash
 
-#docker login
+# docker login
 
-#docker tag dip_video_inpainting:CUDA9-py35-pytorch0.4.1 docker.io/haotianz/dip_video_inpainting:CUDA9-py35-pytorch0.4.1
+# docker tag densepose:py35 docker.io/haotianz/densepose:py35
 
-#docker push haotianz/dip_video_inpainting:CUDA9-py35-pytorch0.4.1
+# docker push haotianz/densepose:py35
